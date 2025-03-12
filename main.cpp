9a7b0d50445291b841d5aab2cfcd41b001776a11
Mika Nokka <mika.nokka1@gmail.com>
2025-03-12 10:59:36 +0200

    Add Dark Theme

--- main.cpp
+++ main.cpp
@@ -1,9 +1,20 @@
 #include <QApplication>
 #include <QMainWindow>
+#include <QFile>
+#include <QTextStream>
 #include "speedometer.h"
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
+
+    QFile file(QCoreApplication::applicationDirPath() + "/../../darktheme.css");
+    if (file.open(QFile::ReadOnly | QFile::Text)) {
+        QTextStream ts(&file);
+        app.setStyleSheet(ts.readAll());
+    } else {
+        qWarning("Tyylitiedoston lataaminen epäonnistui.");
+    }
+
 
     QMainWindow window;
     Speedometer *speedometer = new Speedometer(&window);
